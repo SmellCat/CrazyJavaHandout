5d673e19e10c4403678661297cea15a979a0d73f
Atituset <1433174900@qq.com>
2019-08-12 22:45:31 +0800

    3.4

--- Chapter_3/3_4/IntegerValTest.java
+++ Chapter_3/3_4/IntegerValTest.java
@@ -12,9 +12,14 @@ public class IntegerValTest
 		long bigValue = 99999999999999;
 		                ^
 		1 个错误		
+		
+		
 		*/
-		// long bigValue = 99999999999999;
+		// long bigValue = 99999999999999;  
 		// 下面的代码是正确的，在巨大的整数值后使用L后缀，强制使用long类型
 		long bigValue2 = 9223372036854775807L;
+		// 以0开头的整数值是八进制的整数
+		int octalValue = 013;
+		System.out.println(octalValue);
 	}
 }
