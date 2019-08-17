d8a86e46bf470eea9b704e22ced96f048942bacd
Atituset <1433174900@qq.com>
2019-08-17 22:29:57 +0800

    第三章结束

---
+++ Chapter_3/3_7/AssignOperatorTest.java
@@ -0,0 +1,10 @@
+public class AssignOperatorTest 
+{
+	public static void main(String[] args)
+	{
+		String str = "Java";
+		double pi = 3.14;
+		boolean visited = true;
+		String str2 = str;
+	}
+}
