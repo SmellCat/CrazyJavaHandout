7e836bf2f25b5ca9ab30ea234782859bcc797ba3
Atituset <1433174900@qq.com>
2019-08-18 14:00:04 +0800

    4_4

---
+++ Chapter_4/4_4/ContinueTest2.java
@@ -0,0 +1,20 @@
+public class ContinueTest2
+{
+	public static void main(String[] args)
+	{
+		outer:
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j< 3; j++)
+			{
+				System.out.println("i的值为: " + i + ", j的值为: " + j);
+				if (j == 1)
+				{
+					// continue outer;
+					continue;
+				}
+			
+			}
+		}
+	}
+}
